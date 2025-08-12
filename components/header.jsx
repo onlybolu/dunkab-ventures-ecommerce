244255f5c277367a20bbbffa1d0c0e0fe364c27f
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-08-11 19:27:05 -0700

    new

--- components/header.jsx
+++ components/header.jsx
@@ -171,12 +171,12 @@ const Header = () => {
 
       {/* Mobile Menu */}
       <div
-        className={`fixed top-0 left-0 h-full w-full bg-black/50  transform transition-transform duration-300 ${
+        className={`absolute z-1000 top-0 left-0 h-full w-full bg-black/50  transform transition-transform duration-300 ${
           isMenuOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <div
-          className={`fixed top-0 left-0 h-full w-84 bg-white shadow-2xl transform transition-transform duration-300 ${
+          className={`absolute z-1000 top-0 left-0 h-full w-84 bg-white shadow-2xl transform transition-transform duration-300 ${
             isMenuOpen ? "translate-x-0" : "translate-x-full"
           }`}
         >
