4ed92930a876344bfc9e5d805721967c82d776f4
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-08-11 18:43:40 -0700

    now work

--- src/app/(dashboard)/layout.js
+++ src/app/(dashboard)/layout.js
@@ -1,4 +1,3 @@
-"use client"
 import { Suspense } from "react";
 import Footer from "../../../components/footer";
 import Header from "../../../components/header";
@@ -6,25 +5,22 @@ import Middow from "../../../components/middow";
 import { FavoriteProvider } from "../../../context/FavoriteContext";
 import Script from "next/script";
 
-
-
 export default function RootLayout({ children }) {
   return (
     <div>
       <div className="">
         <Middow />
-        <div className="sticky top-0 z-100">
-            <Header />
-        </div>
-        <FavoriteProvider>
-        <Script src="https://checkout.flutterwave.com/v3.js" strategy="beforeInteractive" />
         <Suspense fallback={<div className="w-full h-screen">Loading.......</div>}>
-
-        {children}
+          <div className="sticky top-0 z-100">
+            <Header />
+          </div>
+          <FavoriteProvider>
+            <Script src="https://checkout.flutterwave.com/v3.js" strategy="beforeInteractive" />
+            {children}
+          </FavoriteProvider>
         </Suspense>
-        </FavoriteProvider>
         <div className="flex flex-col h-full justify-end">
-        <Footer />
+          <Footer />
         </div>
       </div>
     </div>
