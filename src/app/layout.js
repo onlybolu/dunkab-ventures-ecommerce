f5e254698a666e43734e67eee760a5c63d494660
boluwatife obateru <boluwatifeobateru@gmail.com>
2025-08-18 12:52:05 -0700

    pray

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { CartProvider } from "../../context/cartContext";
-import { FavoriteProvider } from "../../context/FavoriteContext"
+import { FavoriteProvider } from "../../context/FavoriteContext";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,30 +17,35 @@ export const metadata = {
   title: "Dunkab Ventures",
   description:
     "At Dunkab, we specialize in providing high quality America coolers, stylish bags, unique souvenirs & kitchen sets",
+  alternates: {
+    canonical: 'https://dunkabventures.com',
+  },
+};
+
+export const jsonLd = {
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "Dunkab Ventures",
+  "url": "https://dunkabventures.com",
+  "logo": "https://dunkabventures.com/logo.png",
 };
 
 export default function RootLayout({ children }) {
-  const logoSchema = {
-    "@context": "https://schema.org",
-    "@type": "Organization",
-    "url": "https://dunkabventures.com",
-    "logo": "https://dunkabventures.com/logo.png" // make sure this URL is correct & accessible
-  };
-
   return (
     <html lang="en">
       <head>
+        <link rel="icon" href="/logo.png" sizes="any" />
         <script
           type="application/ld+json"
-          dangerouslySetInnerHTML={{ __html: JSON.stringify(logoSchema) }}
+          dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
         />
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <CartProvider>
-        <FavoriteProvider>
-          {children}
+          <FavoriteProvider>
+            {children}
           </FavoriteProvider>
-          </CartProvider>
+        </CartProvider>
       </body>
     </html>
   );
